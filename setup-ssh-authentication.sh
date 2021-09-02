c1bd6c726d82976b5ca1ca2fe02d98adee2d436d
Quang Do <quang.do@colorado.edu>
2021-09-02 15:13:50 -0600

    Update

--- setup-ssh-authentication.sh
+++ setup-ssh-authentication.sh
@@ -3,9 +3,11 @@
 set -eu -o pipefail
 
 
+
 echo 'Copying orchestrator public keys...'
 
 public_key=$(docker-compose exec -T orchestrator cat /home/orchestrator-user/.ssh/id_rsa.pub)
+
 
 
 echo "Adding orchestrator public keys to app's authorized keys..."
@@ -13,21 +15,11 @@ echo "Adding orchestrator public keys to app's authorized keys..."
 docker-compose exec -u app-user -T app sh -c "echo ${public_key} > /home/app-user/.ssh/authorized_keys"
 
 
+
 echo "Set password for app-user"
 
 docker-compose exec -u root -T app sh -c "echo 'app-user:app-pass' | chpasswd;"
 
-
-
-# echo 'Creating orchestrator private public keys...'
-
-# Quiet mode, no passphrase, always overwrite
-# docker-compose exec -T orchestrator ssh-keygen -t rsa -f /home/orchestrator-user/.ssh/id_rsa -q -N "" <<<y &>/dev/null
-
-
-# echo "Adding app to orchestrator's known hosts..."
-
-# docker-compose exec -T orchestrator sh -c "ssh-keyscan -t rsa app > /home/orchestrator-user/.ssh/known_hosts 2>/dev/null" 
 
 
 echo "DONE!"
