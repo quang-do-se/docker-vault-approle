b6a4fb326f9ad90018f84c2171c197e993aa653a
Quang Do <quang.do.se@gmail.com>
2021-09-22 21:12:49 -0600

    Updte

--- app/files/entrypoint.sh
+++ app/files/entrypoint.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
- 
+set -eux
+
 printf "\n\033[0;44m$(date) ---> Starting the SSH server.\033[0m\n"
- 
+
 service ssh start
 service ssh status
 
 
+# Set up user with matching host's uid and gid (Linux specific issue)
+
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
 
-echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
+echo "UID: $USER_ID, GID: $GROUP_ID"
 
-usermod -u $USER_ID app
-groupmod -g $GROUP_ID app
+groupadd --gid $GROUP_ID --system app;
+useradd --uid $USER_ID --system --shell /bin/bash --create-home --gid app app;
+
+mkdir -p /home/app/.ssh;
+chown -R app:app /home/app
 
 export HOME=/home/app
 
--- orchestrator/files/entrypoint.sh
+++ orchestrator/files/entrypoint.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-set -e
+# Set up user with matching host's uid and gid (Linux specific issue)
 
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
 
-echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
 
-usermod -u $USER_ID orchestrator
-groupmod -g $GROUP_ID orchestrator
+echo "UID: $USER_ID, GID: $GROUP_ID"
+
+groupadd --gid $GROUP_ID --system orchestrator;
+useradd --uid $USER_ID --system --shell /bin/bash --create-home --gid orchestrator orchestrator;
+
+
+echo 'Creating orchestrator private/public key pair...'
+
+mkdir -p /home/orchestrator/.ssh
+ssh-keygen -q -t ed25519 -N '' -f /home/orchestrator/.ssh/id_ed25519 <<<y >/dev/null 2>&1
+
+chown -R orchestrator:orchestrator /home/orchestrator
+
 
 export HOME=/home/orchestrator
+
 
 exec /usr/sbin/gosu orchestrator "$@"
